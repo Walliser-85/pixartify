ababcd9518566a493eaf3b5eb10ae6f0291bd135
Sascha Burgener <39823945+Walliser-85@users.noreply.github.com>
2024-08-04 16:06:44 +0200

    change webooks new

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -4,7 +4,11 @@ import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix";
 
-import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
+import {
+  createUser,
+  deleteUser,
+  updateUser,
+} from "../../../../lib/actions/user.actions";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook gsdffsdf
